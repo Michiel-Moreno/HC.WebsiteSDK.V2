cf96304897e44786f3fb8b44f04e7ad309980d9c
Michiel Moreno <michiel.moreno@hellocustomer.com>
2025-10-15 16:29:53 +0200

    chore: migrate to Rollup v4 with @rollup/* plugins

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,34 +1,45 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import sourceMaps from 'rollup-plugin-sourcemaps';
-import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
-import { uglify } from 'rollup-plugin-uglify';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import { readFileSync } from 'fs';
 
-const pkg = require('./package.json');
+const pkg = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 const libraryName = 'hcWebsiteTouchpoint';
 
 export default {
-  input: `src/index.ts`,
-  output: [
-    {
-      file: pkg.main,
-      name: libraryName,
-      format: 'umd',
-      sourcemap: true,
-    },
-  ],
+  input: 'src/index.ts',
+  output: {
+    file: pkg.main,
+    name: libraryName,
+    format: 'umd',
+    sourcemap: true,
+  },
   external: [],
   watch: {
     include: 'src/**',
   },
   plugins: [
     json(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript({
+      tsconfig: './tsconfig.json',
+      compilerOptions: {
+        declaration: false, // Already handled by separate tsc build
+        sourceMap: true,
+        inlineSources: true,
+        outDir: 'build/umd', // Match Rollup output directory
+      },
+    }),
+    resolve({
+      browser: true,
+    }),
     commonjs(),
-    resolve(),
-    sourceMaps(),
-    uglify(),
+    terser({
+      format: {
+        comments: false,
+      },
+    }),
   ],
 };
